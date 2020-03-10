0b3714665d36c9586080c4d6c3c84ce89464678d
Jonathan Ott <39764934+Joott@users.noreply.github.com>
2020-03-10 17:35:52 +0100

    Add digestion and mass calc functions

--- Scripts/JP04_Digestion_and_mass_calculation.fsx
+++ Scripts/JP04_Digestion_and_mass_calculation.fsx
@@ -1,3 +1,4 @@
+
 #load @"../IfSharp/Paket.Generated.Refs.fsx"
 
 open BioFSharp
@@ -28,15 +29,90 @@ let aaDistributionHis =
     aminoAcidDistribution
     |> Array.map (fun (name,count) -> name.ToString(), count)
     |> Array.sortByDescending snd
-    |> Chart.Bar
+    |> Chart.Column
     |> Chart.withY_AxisStyle "Count"
     |> Chart.withTitle "Amino Acid composition of the <i>Chlamydomonas reinhardtii</i> proteome"
     |> Chart.Show
 
 let digestedProteins =
     sequences
-    |> Array.map (fun fastAItem ->
+    |> Array.mapi (fun i fastAItem ->
         fastAItem.Header,
-        fastAItem.Sequence
-        |> Digestion.createDigestedPeptide
+        Digestion.BioArray.digest Digestion.Table.Trypsin i fastAItem.Sequence
+        //|> Digestion.BioArray.concernMissCleavages 0 0
     )
+
+//digestedProteins
+//|> Array.sortByDescending (fun (a,x) -> x |> Array.maxBy (fun x -> x.PepSequence.Length))
+
+let digestedPeptideMasses =
+    let massFunction:IBioItem -> float = BioItem.initMonoisoMassWithMemP
+    digestedProteins
+    |> Array.collect (fun (header, digestedProtein) ->
+        
+        digestedProtein
+        |> Array.map (fun peptide ->
+            header,
+            peptide.PepSequence
+            |> List.fold (fun acc aa -> 
+                match aa with
+                | AminoAcids.AminoAcid.Mod(a,_) ->
+                    acc + massFunction a
+                | a -> 
+                    acc + massFunction a
+             ) (massFunction ModificationInfo.Table.H2O) 
+        )
+    )
+
+/// Returns current value,array tuple (current, [|prefix; current; suffix|])
+let motivy prefixLength suffixLength (source: 'T []) =    
+    if prefixLength < 0 then invalidArg "prefixLength" "Input must be non negative"
+    if suffixLength < 0 then invalidArg "suffixLength" "Input must be non negative"
+    let windowSize = prefixLength + suffixLength + 1
+
+    Array.init (source.Length) 
+        (fun i ->
+            let motive =
+                Array.init windowSize 
+                    (fun ii -> 
+                        if i+ii < prefixLength || (i+ii-prefixLength) > (source.Length-1) then
+                            None 
+                        else
+                            Some source.[i+ii-prefixLength])
+            source.[i],motive
+        )
+
+
+
+
+let maxPep = 
+    digestedPeptideMasses
+    |> Array.maxBy snd
+    |> fst
+    |> fun h -> sequences |> Seq.find (fun fa -> fa.Header = h)
+    |> fun fa -> fa.Sequence
+
+let digested = 
+    maxPep
+    |> Digestion.BioArray.digest Digestion.Table.Trypsin 0
+
+
+let bigChunk = 
+    digested
+    |> Array.maxBy (fun i -> i.PepSequence.Length)
+
+bigChunk.PepSequence.[960 .. 990]
+|> Array.ofList
+//|> Array.findIndex ((=) AminoAcids.Arg)
+|> Digestion.BioArray.digest Digestion.Table.Trypsin 0
+
+
+maxPep.PepSequence.Length
+
+digestedPeptideMasses |> Array.sortDescending
+
+let massVisualization =
+    Chart.Histogram digestedPeptideMasses
+    |> Chart.withX_AxisStyle "Mass"
+    |> Chart.withY_AxisStyle "Count"
+    |> Chart.Show
