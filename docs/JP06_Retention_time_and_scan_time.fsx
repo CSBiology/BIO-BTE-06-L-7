2812b8b3e78e9d752edede45c8f81cf18773dd87
Jonathan Ott <jonathan.ott1994@gmail.com>
2021-03-10 17:32:50 +0100

    remove html jp06

--- docs/JP06_Retention_time_and_scan_time.fsx
+++ docs/JP06_Retention_time_and_scan_time.fsx
@@ -3,28 +3,27 @@
 
 [![Binder](https://mybinder.org/badge_logo.svg)](https://mybinder.org/v2/gh/CSBiology/BIO-BTE-06-L-7/gh-pages?filepath=JP06_Retention_time_and_scan_time.ipynb)
 
-1. [Retention time and scan time](#Retention-time-and-scan-time)
-    1. [m/z calculation of the digested peptides](#m/z-calculation-of-the-digested-peptides)
-    2. [Determination of peptide hydrophobicity](#Determination-of-peptide-hydrophobicity)
+1. Retention time and scan time
+    1. m/z calculation of the digested peptides
+    2. Determination of peptide hydrophobicity
 *)
 
 (**
-## Retention time and scan time
-<a href="#Retention-time-and-scan-time" style="display: inline-block"><sup>&#8593;back</sup></a><br>
 
-<div class="container">
+
+
+## Retention time and scan time
+
 In general, peptides are separated by one or more steps of liquid chromatography (LC). The retention time (RT) is the time when the measured 
 peptides were eluting from the column and is therefore influenced by the physicochemical interaction of the particular peptide with the 
 column material. Scan time is basically synonym to retention time, but more from the point of view of the device.
 
 The aim of this notebook is to understand that even though peptides are roughly separated by the LC, multiple peptides elute at the same 
-retention time and are recorded within one MS<sup>1</sup> spectrum. Here, we will simulate a MS<sup>1</sup> spectrum by random sampling from 
+retention time and are recorded within one MS1 spectrum. Here, we will simulate a MS1 spectrum by random sampling from 
 our previously generated peptide-mass distribution. Further, we will try to improve our simulation by incorporating information about the peptide 
-hydrophobicity. It is a only a crude model, but considers the fact that less hydrophobic peptides elute faster from the <sup>13</sup>C LC column.
+hydrophobicity. It is a only a crude model, but considers the fact that less hydrophobic peptides elute faster from the 13C LC column.
 
 As always, we start by loading our famous libraries.
-
-</div>
 *)
 
 #r "nuget: FSharp.Stats, 0.4.0"
@@ -50,12 +49,8 @@ open FSharp.Stats
 
 (**
 ## m/z calculation of the digested peptides
-<a href="#Retention-time-and-scan-time" style="display: inline-block"><sup>&#8593;back</sup></a><br>
 
-<div class="container">
-I think you remember the protein digestion process from the privious notebook (see: <a href="JP04_Digestion_and_mass_calculation.ipynb">JP04 
-Digestion and mass calculation</a>). This time we also remember the peptide sequence, because we need it later for hydrophobicity calculation. 
-</div>
+I think you remember the protein digestion process from the privious notebook (see: *JP04_Digestion_and_mass_calculation.ipynb* ). This time we also remember the peptide sequence, because we need it later for hydrophobicity calculation. 
 *)
 
 // Code-Block 1
@@ -140,7 +135,7 @@ chargedPeptideChar |> GenericChart.toChartHTML
 
 This looks quite strange. I think you immediately see that we forgot about our isotopic cluster. A peptide doesn’t produce a single peak, 
 but a full isotopic cluster. Therefore, we use our convenience function from the previous notebook 
-(see: <a href="JP05_Isotopic_distribution.ipynb">JP05 Isotopic distributiona</a>).
+(see: *JP05_Isotopic_distribution.ipynb* ).
 
 *)
 
@@ -158,9 +153,6 @@ let generateIsotopicDistribution (charge:int) (f:Formula.Formula) =
     |> List.toArray
         
 generateIsotopicDistribution
-
-(**
-*)
 
 // Code-Block 5
 
@@ -194,15 +186,12 @@ peptidesAndMassesChart |> GenericChart.toChartHTML
 
 (**
 ## Determination of peptide hydrophobicity
-<a href="#Retention-time-and-scan-time" style="display: inline-block"><sup>&#8593;back</sup></a><br>
 
-<div class="container">
-In a MS<sup>1</sup> scan, peptides don't appear randomly. They elute according to their hydrophobicity and other physicochemical properties 
+In a MS1 scan, peptides don't appear randomly. They elute according to their hydrophobicity and other physicochemical properties 
 from the LC.
 
-To more accurately represent a MS<sup>1</sup> spectrum, we determine the hydrophobicity of each peptide. Therefore, we first need a function 
+To more accurately represent a MSU+00B9 spectrum, we determine the hydrophobicity of each peptide. Therefore, we first need a function 
 that maps from sequence to hydrophobicity.
-</div>
 *)
 
 // Code-Block 6
@@ -221,9 +210,6 @@ let toHydrophobicity (peptide:AminoAcids.AminoAcid[]) =
     |> Array.average
 
 toHydrophobicity
-
-(**
-*)
 
 // Code-Block 7
 
@@ -274,19 +260,3 @@ peptidesFirst200Chart
 (***hide***)
 peptidesFirst200Chart |> GenericChart.toChartHTML
 (***include-it-raw***)
-
-(**
-<hr>
-<nav class="level is-mobile">
-    <div class="level-left">
-        <div class="level-item">
-            <button class="button is-primary is-outlined" onclick="location.href='/JP05_Isotopic_distribution.html';">&#171; JP05</button>
-        </div>
-    </div>
-    <div class="level-right">
-        <div class="level-item">
-            <button class="button is-primary is-outlined" onclick="location.href='/JP07_Signal_detection_and_quantification.html';">JP07 &#187;</button>
-        </div>
-    </div>
-</nav>
-*)
