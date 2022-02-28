3724cc2b2b83b2382b71559c9ed271514300ffb0
omaus <omaus@bio.uni-kl.de>
2022-03-01 00:06:58 +0100

    Update GitHub image links, add <br>s

--- docs/NB06a_Research_Data_Management.fsx
+++ docs/NB06a_Research_Data_Management.fsx
@@ -14,15 +14,15 @@
 *)
 
 (**
-The NFDI (Nationale Forschungsdateninfrastruktur) is a german infrastructure that targets standardization of RDM. The CSB group is part of DataPLANT, a consortium within the NFDI, which tackles the field of basic plant research. To achieve the already introduced FAIR principles, DataPLANT envisioned the **Annotated Research Context** (short: **ARC**\).
+The NFDI (Nationale Forschungsdateninfrastruktur) is a german infrastructure that targets standardization of Research Data Management (RDM). The CSB group is part of DataPLANT, a consortium within the NFDI, which tackles the field of basic plant research. To achieve the already introduced FAIR principles, DataPLANT envisioned the **Annotated Research Context** (short: **ARC**\).
 
 # ARC
 
-![](ARC.png)
+![](https://raw.githubusercontent.com/CSBiology/BIO-BTE-06-L-7/main/docs/img/ARC.png)
 
 The ARC is, basically, a folder and file structure. Its design shall cover the entire research cycle, from experimental to computational analyses, actual data and metadata, as well as resulting publications.
 
-![](ARCfolderStructure.jpg)  
+![](https://github.com/CSBiology/BIO-BTE-06-L-7/raw/main/docs/img/ARCfolderStructure.jpg)  
 ***Figure 8: An exemplary ARC, schematically visualised***
 
 Some parts of the ARC are an implementation of the ISA model you have seen before, e.g. the _Investigation_ file, _Study_ files, and the _assays_ folder.  
@@ -34,10 +34,11 @@ Finally, there's the _Investigation_ file which is located in the ARC's root fol
 
 # ISA-Xlsx
 
-The _Assay_ files are, from an experimenter's perspective, the most important files of each ARC since they contain the information about 
-- the sample that was used
-- the condition of the sample, i.e. how the sample was treated
-- the way how the data was obtained from the sample
+The _Assay_ files are, from an experimenter's perspective, the most important files of each ARC since they contain the information about
+
+  - the sample that was used
+  - the condition of the sample, i.e. how the sample was treated
+  - the way how the data was obtained from the sample
 
 In each _Assay_ file, the metadata of the respective experiment is documented thoroughly. To do so, the Xlsx file format is suitable. The common way to work with Xlsx files are spreadsheet programs like Microsoft Excel or LibreOffice.  
 
@@ -45,21 +46,20 @@ In each _Assay_ file, the metadata of the respective experiment is documented th
 
 A practical implementation of the [ISA standard](https://isa-specs.readthedocs.io/en/latest/isamodel.html) is [ISA-Xlsx](https://github.com/nfdi4plants/ARC-specification/blob/main/ARC%20specification.md#isa-xlsx-format). In this, the [format of the Assay files](https://isa-specs.readthedocs.io/en/latest/isatab.html#study-and-assay-files) served as the blueprint. Here, ISA is realized in a very similar manner.
 
-A practical implementation of the [ISA standard](https://isa-specs.readthedocs.io/en/latest/isamodel.html) is [ISA-Xlsx](https://github.com/nfdi4plants/ARC-specification/blob/main/ARC%20specification.md#isa-xlsx-format). In this, the [format of the Assay files](https://isa-specs.readthedocs.io/en/latest/isatab.html#study-and-assay-files) served as the blueprint. Here, ISA is realized in a very similar manner.
-
 An _Assay_ file consists of at least one annotation table (like in Figure 9). 1 annotation table per Xlsx worksheet is allowed. Each annotation table represents an ISA process which is the depiction of a sequence of steps from the input to the output.  
 Annotation tables are row-oriented. This means that the first row consists entirely of each column's header. Annotation tables are ordered from left to right and always begin with a source column ("Source Name") and end with a sink column ("Sample Name" or "Data File Name"). In between are _Building Blocks_ that consist of an ISA term ("Characteristics", "Parameter", or "Factor") and an ontology term in square brackets.  
 The cells below the headers are the values.
 
-![](ExemplaryAssayFile.png)  
+![](https://github.com/CSBiology/BIO-BTE-06-L-7/raw/main/docs/img/ExemplaryAssayFile.png)  
 ***Figure 9: An annotation table of an exemplary Assay file.***
 
 The different types of columns:
+
 - the source column: **Source Name**  
 The values of this column describe the name of the _input_ of the process. This can be, in a lab experiment, the name of the biological source, e.g. the name of a specimen in a petri dish.
 - the sink columns
   - **Sample Name**  
-    Sample names describe the name of the _output_ of a process. For example, if a process ends in the specimen being stored in an eppendorfer tube, it could be the name of this eppi.
+    A Sample Name describes the name of the _output_ of a process. For example, if a process ends in the specimen being stored in an eppendorfer tube, it could be the name of this eppi.
   - **Data File Name**  
     Like the Sample Name except that it is used when the output is a file. The whole file's name shall be used as Data File Name, that also includes the file extension.
 - the _Building Blocks_:
@@ -70,12 +70,12 @@ The values of this column describe the name of the _input_ of the process. This 
   - **Factor**  
     Factors are specific _Building Blocks_ that can be Parameters of Characteristics of special importance in this experiment or research project. We won't use them in this practical course.
 
-![](ParameterBuildingBlock_filled.png)  
+![](https://github.com/CSBiology/BIO-BTE-06-L-7/raw/main/docs/img/ParameterBuildingBlock_filled.png)  
 ***Figure 11: Building Block "Parameter [instrument model]", filled with values.*** 
 
-Ideally, an annotation table describes this experiment's process chronologically so that a researcher trying to replicate it can easily do so. The worksheets an _Assay_ file consists of, should be ordere chronologically, too.  
+Ideally, an annotation table describes its experiment's process chronologically so that a researcher trying to replicate it can easily do so. The worksheets an _Assay_ file consists of should be ordered chronologically, too.  
 
-While you could do all this stuff by hand, it is much easier of you use a tool that automatizes much of the work. For this, an MS Excel add-in exists that facilitates the work with Xlsx files massively.
+While you could do all this stuff by hand, it is much easier if you use a tool that automatizes much of the work. For this, an MS Excel add-in exists that facilitates the work with Xlsx files massively.
 
 # Swate
 
@@ -85,23 +85,26 @@ You either need to have Microsoft Office installed to work with the desktop appl
 Header over to the [installing chapter for Swate](https://github.com/nfdi4plants/Swate#installuse) and follow the instructions there.
 
 After you successfully installed Swate, you can create an exemplary _Assay_ file to get a feeling of how to use Swate.  
-1. Create a new Xlsx file and open it (online or desktop)
+
+1. Create a new Xlsx file and open it (online or desktop)  
 2. Start Swate Core by opening it via the Data tab  
-![](OpenSwateCore.png)
+![](https://github.com/CSBiology/BIO-BTE-06-L-7/raw/main/docs/img/OpenSwateCore.png)  
 3. [Create a new annotation table](https://github.com/nfdi4plants/Swate/wiki/Docs02-Annotation-Table)  
-![](CreateAnnotationTable.jpg)
+![](https://github.com/CSBiology/BIO-BTE-06-L-7/raw/main/docs/img/CreateAnnotationTable.jpg)  
 4. [Add Building Blocks](https://github.com/nfdi4plants/Swate/wiki/Docs03-Building-Blocks)  
-![](AddBuildingBlock.jpg)  
-Search the term database (B) for a term fitting. If you don't find one, just write a custom term that suits your needs.  
+![](https://github.com/CSBiology/BIO-BTE-06-L-7/raw/main/docs/img/AddBuildingBlock.jpg)  
+5. Search the term database (B) for a term fitting. If you don't find one, just write a custom term that suits your needs.  
 You can add units to them before creation (D) or afterwards:  
-![](UpdateUnit.jpg)
-5. [Fill cells with terms](https://github.com/nfdi4plants/Swate/wiki/docs04-Ontology-Term-Search)  
-![](TermInsert.jpg)  
+<br>
+![](https://github.com/CSBiology/BIO-BTE-06-L-7/raw/main/docs/img/UpdateUnit.jpg)  
+6. [Fill cells with terms](https://github.com/nfdi4plants/Swate/wiki/docs04-Ontology-Term-Search)  
+![](https://github.com/CSBiology/BIO-BTE-06-L-7/raw/main/docs/img/TermInsert.jpg)  
+<br>
 Like with Building Blocks, rather write down your own terms before choosing a term from the database that doesn't fit well and annotates the experiment worse.
-6. You can also type values into cells without using the Ontology Term Search, but don't forget to update them afterwards via the _Update Ontology Terms_ function:  
-![](UpdateOntologyTerms.png)
-7. Use _Autoformat Table_ (A) to automatically format an annotation table. _Remove Building Block_ (B) deletes whole Building Blocks while _Get Building Block Information_ gives you information about the terms and size of a Building Block:
-![](AutoformatTable_RemoveBuildingBlock_GetBuildingBlockInformation.png)
+7. You can also type values into cells without using the Ontology Term Search, but don't forget to update them afterwards via the _Update Ontology Terms_ function:  
+![](https://github.com/CSBiology/BIO-BTE-06-L-7/raw/main/docs/img/UpdateOntologyTerms.png)
+8. Use _Autoformat Table_ (A) to automatically format an annotation table. _Remove Building Block_ (B) deletes whole Building Blocks while _Get Building Block Information_ gives you information about the terms and size of a Building Block:  
+![](https://github.com/CSBiology/BIO-BTE-06-L-7/raw/main/docs/img/AutoformatTable_RemoveBuildingBlock_GetBuildingBlockInformation.png)
 
 # Editing an ARC
 
